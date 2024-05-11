088eebe488c214cbfb3678e2cb783c518e33bae6
Turgay Ceylan <90829101+turgay2317@users.noreply.github.com>
2024-05-11 18:05:14 +0300

    static-routing-and-templates-located

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('templates/<path:template_name>', views.template_view),
+    path('static/<path:path>', views.static_view),
     path("",views.index),
     path("index",views.index),
-
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,23 @@
 from django.http.response import HttpResponse
+from django.conf import settings
 from django.shortcuts import render
-
+from django.contrib.staticfiles import finders
+from django.http import HttpResponseNotFound
 
 # Create your views here.
 def index(request):
     return HttpResponse("homepage")
+
+
+def template_view(request, template_name):
+    try:
+        return render(request, template_name)
+    except:
+        return HttpResponseNotFound("Template not found")
+
+def static_view(request, path):
+    try:
+        with open(f'my_app/static/{path}', 'rb') as f:
+            return HttpResponse(f.read(), content_type='application/octet-stream')
+    except FileNotFoundError:
+        return HttpResponseNotFound("Static file not found")
